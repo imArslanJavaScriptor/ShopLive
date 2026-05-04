0d48be2bc5e8dfa6f43d81ec7663baf922e3a049
Muhammad Arslan <imarslan7061@gmail.com>
2026-05-05 02:23:54 +0500

    implement checkout functionality with validation and integration with Polar API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,7 @@ import keepAliveCroneJob from "./lib/cron";
 import meRouter from "./routes/meRouter";
 import productRouter from "./routes/productRouter";
 import streamRouter from "./routes/streamRouter";
+import checkoutRouter from "./routes/checkoutRouter"
 
 const env = getEnv()
 const app = express()
@@ -25,6 +26,10 @@ app.post("/webhooks/clerk", rawJson, (req, res) => {
   void clerkWebhookHandler(req, res)
 })
 
+// app.post("/webhooks/polar", rawJson, (req, res) => {
+//   void polarWebhookHandler(req, res)
+// })
+
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
@@ -36,6 +41,7 @@ app.get("/health", (_req, res) => {
 app.use("/api/me", meRouter)
 app.use("/api/products", productRouter)
 app.use("/api/stream", streamRouter)
+app.use("/api/checkout", checkoutRouter)
 
 const publicDir = path.join(process.cwd(), "public")
 if(fs.existsSync(publicDir)) {
